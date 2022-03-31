d7fab3967d4ed2c656bd6617e58ef478534601ad
Kit Ko <kitko112@gmail.com>
2022-03-31 16:14:55 +0800

    include region on s3 initialization

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -3,15 +3,16 @@ import mime from "mime-types";
 import S3 from "aws-sdk/clients/s3";
 import { PresignedUrl } from '@/interfaces/file.interface';
 
-const URL_EXPIRATION_SECONDS = 300;
-const BUCKET_NAME='twbc-nft-images';
-const DOMAIN_NAME='twbc-nft-images.s3.cn-northwest-1.amazonaws.com.cn';
+const URL_EXPIRATION_SECONDS = 3600;
+const BUCKET_NAME = 'twbc-nft-images';
+const DOMAIN_NAME = 'twbc-nft-images.s3.cn-northwest-1.amazonaws.com.cn';
+const REGION = 'cn-northwest-1';
 
 class FileService {
 
   public async getPresignedUrl(contentType: string): Promise<PresignedUrl> {
 
-    const s3 = new S3();
+    const s3 = new S3({ region: REGION });
     const fileKey = `${uuid()}.${mime.extension(contentType)}`;
 
     const uploadUrl = await s3.getSignedUrlPromise("putObject", {
