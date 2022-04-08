601df76c8940fea74a221f23607320158d3a9e65
xudongdong <dongdong.xu1@thoughtworks.com>
2022-04-08 15:00:24 +0800

    feat: add get my mint nft api

---
+++ src/apis/mintRecords.api.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from 'express';
+import db from '@databases';
+
+const api = Router();
+
+const baseUrl = '/mintRecords';
+
+api.get(baseUrl, async (req: Request, res: Response) => {
+  const { email } = req.body;
+  const user = await db.user.findUnique({
+    where: { email },
+    include: {
+      mintRecord: {
+        include: {
+          collection: {
+            include: {
+              resource: true,
+              subject: true
+            }
+          },
+        },
+      },
+    },
+  });
+
+  res.json(user.mintRecord)
+});
+
+export { api };
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import AppRouter from './router';
 import { api as denomAPI } from './apis/denoms.api';
 import { api as collectionAPI } from './apis/collections.api';
 import { api as authAPI} from './apis/auth.api';
+import { api as mintRecordsAPI} from './apis/mintRecords.api';
 
-AppRouter.use(denomAPI, collectionAPI, authAPI);
+AppRouter.use(denomAPI, collectionAPI, authAPI, mintRecordsAPI);
 
 expressListRoutes(AppRouter);
 const app = new App(AppRouter);
